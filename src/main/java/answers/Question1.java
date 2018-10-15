43230fe20ff13bfb8aab211ffaa129d2eb587ff0
Hubert <hb12@o2.pl>
2018-10-15 22:15:01 +0100

    q1 first try

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,33 @@
 package answers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		
+		 int max = 0, tmp = 0;
+	        for(int i = 31; i >= 0; i--){
+	        	tmp = tmp|(1<<i);
+	            
+	            Set<Integer> s = new HashSet<>();
+	            
+	            for(int num : portfolios){
+	                s.add(num&tmp);
+	            }
+	            
+	            int txor = max|(1<<i);
+	            
+	            for(int pre : s)
+	                if(s.contains(txor ^ pre)) {
+	                    max = txor; break;
+	                }
+	            
+	        }
+	        return max;
 	}
+
+	
 
 }
